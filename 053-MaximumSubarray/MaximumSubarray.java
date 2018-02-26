969c3f113ce4a34853bcfa3a4bfcff73f6a83852
Emre Cosar <emrecosarr@gmail.com>
2018-02-26 23:38:37 +0300

    53 MaximumSubarray

---
+++ 053-MaximumSubarray/MaximumSubarray.java
@@ -0,0 +1,10 @@
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0], maxEndingHere = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+}
