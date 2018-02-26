83c647a576a6813117fea580052acf436a0986b1
Emre Cosar <emrecosarr@gmail.com>
2018-02-27 00:14:49 +0300

    198 House Robber

---
+++ 198-HouseRobber/HouseRobber.java
@@ -0,0 +1,12 @@
+public class HouseRobber {
+    public int rob(int[] nums) {
+        int prevNo = 0;
+        int prevYes = 0;
+        for (int n : nums) {
+            int temp = prevNo;
+            prevNo = Math.max(prevNo, prevYes);
+            prevYes = n + temp;
+        }
+        return Math.max(prevNo, prevYes);
+    }
+}
