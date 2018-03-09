bafe6912553e558c6a4c3de8965d067136aa73ea
Emre Cosar <emrecosarr@gmail.com>
2018-03-10 00:11:20 +0300

    204 CountPrimes with optimization

--- 204-CountPrimes/CountPrimes.java
+++ 204-CountPrimes/CountPrimes.java
@@ -1,24 +1,16 @@
 public class CountPrimes {
     public int countPrimes(int n) {
-        if (n < 3)
-            return 0;
-        List<Integer> primes = new ArrayList<Integer>();
-        primes.add(2);
-
-        for (int i = 3; i < n; i++) {
-            boolean status = true;
-            for (int j = 0; j < primes.size(); j++) {
-                int current = primes.get(j);
-                if (i % current == 0) {
-                    status = false;
-                    break;
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i] == false) {
+                count++;
+                for (int j = 2; i * j < n; j++) {
+                    notPrime[i * j] = true;
                 }
             }
-            if (status)
-                primes.add(i);
         }
 
-        return primes.size();
-
+        return count;
     }
 }
