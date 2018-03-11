4aab304773eaf29679a6b912e8c6b69f73a04352
Emre Cosar <emrecosarr@gmail.com>
2018-03-11 14:52:54 +0300

    238 Product of Array Itself

---
+++ 238-ProductOfArrayItself/ProductOfArrayItself.java
@@ -0,0 +1,15 @@
+public class ProductOfArrayItself {
+    public int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        output[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            output[i] = output[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
+        }
+        return output;
+    }
+}
