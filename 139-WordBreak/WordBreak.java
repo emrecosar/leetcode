f8873261beccdd88e6589160f6d1cd28df61e7ba
Emre Cosar <emrecosarr@gmail.com>
2018-04-12 00:31:45 +0300

    Word bread DP solution fix

--- 139-WordBreak/WordBreak.java
+++ 139-WordBreak/WordBreak.java
@@ -1,13 +1,20 @@
 public class WordBreak {
     public boolean wordBreak(String s, List<String> wordDict) {
+
+        boolean[] status = new boolean[s.length() + 1];
+        status[0] = true;
+
+
         for(int i = 1; i <= s.length(); i++){
-            if(wordDict.contains(s.substring(0, i))){
-                if(i == s.length())
-                    return true;
-                if(wordBreak(s.substring(i), wordDict))
-                    return true;
+            for(int j = 0; j < i; j++){
+                if(status[j] && wordDict.contains(s.substring(j,i))){
+                    status[i] = true;
+                    break;
+                }
             }
         }
-        return false;
+
+        return status[s.length()];
+        
     }
 }
